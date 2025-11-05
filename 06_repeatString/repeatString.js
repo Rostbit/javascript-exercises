6b13f2fb7fe8d4f0e4f809b9e49db89430e36dd9
Rostbit <140731825+Rostbit@users.noreply.github.com>
2025-11-05 12:02:18 +0300

    Create a repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR";
+  let finalString = ``;
+  for (let index = 0; index < num; index++) {
+    finalString += string;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
+
 module.exports = repeatString;
