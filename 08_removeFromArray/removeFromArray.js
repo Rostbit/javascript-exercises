95beca5f2b62ea291f9a8edf405e8eac34d85732
Rostbit <140731825+Rostbit@users.noreply.github.com>
2025-11-07 13:08:21 +0300

    Create a removeFromArray function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+  return array.filter((value) => !items.includes(value));
 };
 
 // Do not edit below this line
