ac5b6ae8517700e21391191f7fa8d8b5d5e224ba
Rostbit <140731825+Rostbit@users.noreply.github.com>
2025-11-07 16:01:40 +0300

    Create sumAll function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return "ERROR";
+  } else if (a < b) {
+    let sum = 0;
+    for (let index = a; index <= b; index++) {
+      sum += index;
+    }
+    return sum;
+  } else if (b < a) {
+    let sum = 0;
+    for (let index = b; index <= a; index++) {
+      sum += index;
+    }
+    return sum;
+  } else return a;
 };
 
 // Do not edit below this line
